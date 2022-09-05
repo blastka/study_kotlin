f4cf9c0e455a3159ea0a46df6f86d7d2bcddf544
blastka505 <necturna505@gmail.com>
2022-09-05 12:52:03 +0300

    добавлены примеры

--- app/src/main/java/com/example/stydy_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/stydy_kotlin/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.stydy_kotlin
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
+import com.example.myapplication.presentation.example8.TestGson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val test = TestGson()
+        test.main(applicationContext)
 
     }
 
