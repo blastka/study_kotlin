b3c6b3a9bc96aa08c58f0563aa13ca4d6a24491b
blastka505 <necturna505@gmail.com>
2022-09-27 15:19:30 +0300

    Примеры

---
+++ app/src/main/java/com/example/stydy_kotlin/presentation/example1/Test1.kt
@@ -0,0 +1,11 @@
+package com.example.stydy_kotlin.presentation.example1
+
+import kotlin.experimental.or
+
+fun main(){
+    var byte: Byte = 5
+    var byte2: Byte = 7
+    var result = byte or byte2
+    println(result)
+}
+
---
+++ app/src/main/java/com/example/stydy_kotlin/presentation/example1/Test2.kt
@@ -0,0 +1,11 @@
+package com.example.stydy_kotlin.presentation.example1
+
+fun main(){
+    A().print()
+}
+
+class A(){
+    fun print(){
+        println("я запустился")
+    }
+}
